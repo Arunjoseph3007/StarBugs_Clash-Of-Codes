ec0ee6dc7e957497be620eeb6dd0082ec9f93ae7
Anup Joseph <arunjoseph3007@gmail.com>
2023-03-04 22:26:35 +0530

    Edit Profile UI refactor

--- src/components/Verify.js
+++ src/components/Verify.js
@@ -15,8 +15,21 @@ export default function Verify({ show, close }) {
   }
   return (
     <>
-      <FormControl bgColor={"gray.200"} width={"50vw"} as="Center" p={"10px"} m='5px'>
-      <CloseIcon onClick={close} display='inline' position={'absolute'} left='95%'p={'2px'}/>
+      <FormControl
+        bgColor={"gray.200"}
+        width={""}
+        rounded="md"
+        as="Center"
+        p={"10px"}
+        m="5px"
+      >
+        <CloseIcon
+          onClick={close}
+          display="inline"
+          position={"absolute"}
+          left="95%"
+          p={"2px"}
+        />
         <Stack>
           <Box>
             <FormLabel display={"inline"}>Aadhar Number</FormLabel>
@@ -34,7 +47,7 @@ export default function Verify({ show, close }) {
             <Input type={"file"} w="20vw" />
           </Box>
         </Stack>
-        <Button colorScheme="facebook" w={"10vw"} type="submit" my={'2px'}>
+        <Button colorScheme="facebook" w={"10vw"} type="submit" my={"2px"}>
           Submit
         </Button>
       </FormControl>
--- src/pages/ProfileForm.js
+++ src/pages/ProfileForm.js
@@ -11,6 +11,7 @@ import {
   Box,
   Heading,
   Button,
+  Center,
 } from "@chakra-ui/react";
 
 export default function ProfileForm() {
@@ -32,21 +33,21 @@ export default function ProfileForm() {
     // console.log(data)
 
     let data = {
-      "user": "3",
-      "name": {name},
-      "interests": {interest},
-      "dob": {dob},
-      "location": {location},
-      "gender": {gender}
-    }
-    console.log(data)
+      user: "3",
+      name: { name },
+      interests: { interest },
+      dob: { dob },
+      location: { location },
+      gender: { gender },
+    };
+    console.log(data);
 
     try {
       let headers = {
         "Content-Type": "application/json",
         Authorization:
           "Token 9fb10eb951c9c33a768b4dc475d410f3c2064b928004a2a3aa13a1648f70494c",
-      }
+      };
       let reqOptions = {
         url: "https://coctrinity.pythonanywhere.com/login/profile-create",
         method: "POST",
@@ -66,87 +67,102 @@ export default function ProfileForm() {
       setInterest("");
     }
   }
+  
   return (
-    <Box w="50vw" border={"2px solid black"} display="block">
-      <FormControl colorScheme={"facebook"} encType="multipart/form" as='form'>
-        <Heading textAlign={"center"} as="h4" color={"blue.700"} my="2px">
-          Edit Profile
-        </Heading>
-        <Stack>
-          <Box display={"grid"} gridTemplateColumns="1fr 1fr">
-            <FormLabel display={"inline"}>Username</FormLabel>
-            <Input
-              placeholder="Username"
-              w="20vw"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Box>
-          <Box display={"grid"} gridTemplateColumns="1fr 1fr">
-            <FormLabel display={"inline"}>Profile Photo</FormLabel>
-            <input type="file" />
-          </Box>
-          <Box>
-            <FormLabel>Interests</FormLabel>
-            <Textarea
-              placeholder="Interests"
-              id="interestsInput"
-              value={interest}
-              onChange={(e) => setInterest(e.target.value)}
-            />
-          </Box>
-          <Box display={"grid"} gridTemplateColumns="1fr 1fr">
-            <FormLabel display={"inline"}>Location</FormLabel>
-            <Input
-              placeholder="Location"
-              w="20vw"
-              type="text"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </Box>
-          <Box display={"grid"} gridTemplateColumns="1fr 1fr">
-            <FormLabel display={"inline"}>Date of Birth</FormLabel>
-            <Input
-              w="20vw"
-              type="date"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            />
-          </Box>
-          <Box display={"grid"} gridTemplateColumns="1fr 1fr">
-            <FormLabel display={"inline"}>Gender</FormLabel>
-            <Input
-              w="20vw"
-              type="text"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-            />
-          </Box>
-          <Box>
-            <FormLabel display={"inline"}>Verify your Profile</FormLabel>
-            <Button
-              colorScheme={"facebook"}
-              variant="outline"
-              onClick={() => {
-                setShowModal(true);
-              }}
-            >
-              Verify
+    <Center my={20} minH="100vh">
+      <Box
+        w="50vw"
+        shadow='xl'
+        border={"2px"}
+        borderColor="gray.100"
+        p={4}
+        rounded="md"
+        display="block"
+      >
+        <FormControl
+          colorScheme={"facebook"}
+          encType="multipart/form"
+          as="form"
+        >
+          <Heading fontFamily='Poppins' textAlign={"center"} as="h4" color={"#0652cf"} my={4}>
+            Edit Profile
+          </Heading>
+          <Stack>
+            <Box display={"grid"} gridTemplateColumns="1fr 1fr">
+              <FormLabel display={"inline"}>Username</FormLabel>
+              <Input
+                placeholder="Username"
+                w="20vw"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Box>
+            <Box display={"grid"} gridTemplateColumns="1fr 1fr">
+              <FormLabel display={"inline"}>Profile Photo</FormLabel>
+              <input type="file" />
+            </Box>
+            <Box>
+              <FormLabel>Interests</FormLabel>
+              <Textarea
+                placeholder="Interests"
+                id="interestsInput"
+                value={interest}
+                onChange={(e) => setInterest(e.target.value)}
+              />
+            </Box>
+            <Box display={"grid"} gridTemplateColumns="1fr 1fr">
+              <FormLabel display={"inline"}>Location</FormLabel>
+              <Input
+                placeholder="Location"
+                w="20vw"
+                type="text"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </Box>
+            <Box display={"grid"} gridTemplateColumns="1fr 1fr">
+              <FormLabel display={"inline"}>Date of Birth</FormLabel>
+              <Input
+                w="20vw"
+                type="date"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+              />
+            </Box>
+            <Box display={"grid"} gridTemplateColumns="1fr 1fr">
+              <FormLabel display={"inline"}>Gender</FormLabel>
+              <Input
+                w="20vw"
+                type="text"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <FormLabel display={"inline"}>Verify your Profile</FormLabel>
+              <Button
+                colorScheme={"facebook"}
+                variant="outline"
+                onClick={() => {
+                  setShowModal(true);
+                }}
+              >
+                Verify
+              </Button>
+              <Verify
+                show={showModal}
+                close={() => {
+                  setShowModal(false);
+                }}
+              />
+            </Box>
+            <Button colorScheme="facebook" onClick={submitForm}>
+              Submit
             </Button>
-            <Verify
-              show={showModal}
-              close={() => {
-                setShowModal(false);
-              }}
-            />
-          </Box>
-          <Button colorScheme="facebook" onClick={submitForm}>
-            Submit
-          </Button>
-        </Stack>
-      </FormControl>
-    </Box>
+          </Stack>
+        </FormControl>
+      </Box>
+    </Center>
   );
 }
