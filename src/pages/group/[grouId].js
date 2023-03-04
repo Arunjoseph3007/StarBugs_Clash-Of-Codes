76838b0f97f16cb92a3f4cc4f0f6be6d57039a5c
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-03-04 21:26:02 +0530

    Group Id done bhai

--- src/pages/group/[grouId].js
+++ src/pages/group/[grouId].js
@@ -12,11 +12,21 @@ import {
   Button,
   Grid,
   GridItem,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Lorem,
 } from "@chakra-ui/react";
 import Navbar from "@/components/Navbar";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { MdLocationOn, MdOutlineLocationCity } from "react-icons/md";
 import { BsCalendarDate, BsFillCalendarDateFill } from "react-icons/bs";
+import { SiYourtraveldottv } from "react-icons/si";
 import Link from "next/link";
 const Feature = ({ text, icon, iconBg }) => {
   return (
@@ -44,13 +54,18 @@ export default function SplitWithImage() {
     startDate: "12/05/2023",
     endDate: "22/05/2023",
     People: 25,
-    Details: "Padharo maare dessh presents Kutch tour for all DJites",
+    Details:
+      "Padharo maare dessh presents Kutch tour for all DJites, Come with us and have fun.",
     imageurl: "/kutch.png",
     isInterested: false,
-    estimateCost:"₹25,000/-pp",
-    vcurl:"https://chal-mere-yaar.whereby.com/example-prefix513d5e4d-9918-4ce3-863b-66a98cbac233?roomKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWV0aW5nSWQiOiI3MDc5MTI1OSIsInJvb21SZWZlcmVuY2UiOnsicm9vbU5hbWUiOiIvZXhhbXBsZS1wcmVmaXg1MTNkNWU0ZC05OTE4LTRjZTMtODYzYi02NmE5OGNiYWMyMzMiLCJvcmdhbml6YXRpb25JZCI6IjE3OTY0NyJ9LCJpc3MiOiJodHRwczovL2FjY291bnRzLnNydi53aGVyZWJ5LmNvbSIsImlhdCI6MTY3NzkzMzYxNywicm9vbUtleVR5cGUiOiJtZWV0aW5nSG9zdCJ9.h-Uad-TMu4TGisXiC0mxFK6Df0tpPSfP4oqy4yIjhaY",
+    estimateCost: "25000",
+    mode: "Road",
+    vcurl:
+      "https://chal-mere-yaar.whereby.com/example-prefix513d5e4d-9918-4ce3-863b-66a98cbac233?roomKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWV0aW5nSWQiOiI3MDc5MTI1OSIsInJvb21SZWZlcmVuY2UiOnsicm9vbU5hbWUiOiIvZXhhbXBsZS1wcmVmaXg1MTNkNWU0ZC05OTE4LTRjZTMtODYzYi02NmE5OGNiYWMyMzMiLCJvcmdhbml6YXRpb25JZCI6IjE3OTY0NyJ9LCJpc3MiOiJodHRwczovL2FjY291bnRzLnNydi53aGVyZWJ5LmNvbSIsImlhdCI6MTY3NzkzMzYxNywicm9vbUtleVR5cGUiOiJtZWV0aW5nSG9zdCJ9.h-Uad-TMu4TGisXiC0mxFK6Df0tpPSfP4oqy4yIjhaY",
   });
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const btnRef = useRef(null);
   return (
     <Container maxW={"7xl"}>
       <Navbar />
@@ -69,9 +84,10 @@ export default function SplitWithImage() {
             {groupDetails.People} People Interested
           </Text>
           <Heading>{groupDetails.Name}</Heading>
-          <Text color={"gray.500"} fontSize={"lg"}>
-            Es
+          <Text color={"gray.600"} fontSize={"lg"}>
+            {groupDetails.Details}
           </Text>
+
           <Stack
             spacing={4}
             divider={
@@ -122,19 +138,58 @@ export default function SplitWithImage() {
                 />
               </GridItem>
             </Grid>
+            <Feature
+              icon={<Icon as={SiYourtraveldottv} w={5} h={5} />}
+              iconBg={useColorModeValue("blue.100", "blue.900")}
+              text={"By " + groupDetails.mode}
+            />
+
+            <Text color={"gray.900"} fontSize={"lg"}>
+              Estimated Cost of Plan ₹{groupDetails.estimateCost} per person
+              <br />
+              <Text ref={btnRef} onClick={onOpen} color={"black"} as={"ins"}>
+                More Deails
+              </Text>
+            </Text>
+
+            <Modal
+              onClose={onClose}
+              finalFocusRef={btnRef}
+              isOpen={isOpen}
+              scrollBehavior={"inside"}
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Detailed Plan</ModalHeader>
+                
+                <ModalBody>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, sed quasi quia accusantium exercitationem, quos iure nostrum unde fugit, aliquid autem labore sapiente harum tempore mollitia provident similique! Debitis voluptates ipsam, culpa voluptatum fugit vitae dolorum atque sequi, architecto quis perferendis vero ipsa id? Hic voluptatibus ut reiciendis vitae dolorem rem dolore aut, suscipit eos, doloremque inventore voluptate voluptatem quidem reprehenderit temporibus ullam natus id blanditiis culpa quae laboriosam saepe fuga ipsum illo! Quos, consequatur atque animi dolore nemo beatae recusandae corrupti corporis illum laborum sint eligendi saepe sunt repellendus ea voluptates, distinctio ratione? Minus totam voluptas alias non aliquam nihil qui iste facere maxime, cumque nostrum autem mollitia aspernatur odio consequuntur vero impedit nobis quod atque eligendi ratione molestias. Asperiores voluptates tempore sapiente numquam libero! Optio sunt non eos voluptatum minus neque doloremque voluptatibus qui laborum! Eius culpa obcaecati quia distinctio necessitatibus facilis, aspernatur quos suscipit maiores repellat iste. In ullam dolores dignissimos maiores, autem voluptate ipsum necessitatibus deleniti optio a! Harum, nisi inventore? Sed, tempore maiores quae cumque, id eveniet quis ipsum minus, animi excepturi quos delectus! Accusamus sit commodi unde cupiditate id, provident, numquam est suscipit sequi labore maiores consequatur laudantium odio hic fugit saepe asperiores veniam?
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={onClose} colorScheme={"facebook"}>Close</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
           </Stack>
           <Flex gap={4}>
-            <Button
+            {groupDetails.isInterested?<Button
+              colorScheme={"facebook"}
+              background={"#00215e"}
+              variant={"solid"}
+              disabled={true}
+            >
+              Already Interested ✓
+            </Button>:<Button
               colorScheme={"facebook"}
               background={"#0652cf"}
               variant={"solid"}
             >
               Interested!
-            </Button>
+            </Button>}
             <Link href={groupDetails.vcurl} target="_blank">
-            <Button colorScheme={"facebook"} variant={"outline"} >
-              Join VC
-            </Button>
+              <Button colorScheme={"facebook"} variant={"outline"}>
+                Join VC
+              </Button>
             </Link>
           </Flex>
         </Stack>
