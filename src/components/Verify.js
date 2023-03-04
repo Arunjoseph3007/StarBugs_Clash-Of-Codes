f2195572fddf393e52987b3283eea5761b5a3eb3
Sahil Nair <sahilnair329@gmail.com>
2023-03-04 15:09:45 +0530

    Profileform

---
+++ src/components/Verify.js
@@ -0,0 +1,44 @@
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Box,
+    Button,
+  } from "@chakra-ui/react";
+  import { CloseIcon } from "@chakra-ui/icons";
+  import React from "react";
+  
+  export default function Verify({ show, close }) {
+    if (!show) {
+      return null;
+    }
+    return (
+      <>
+        <FormControl bgColor={"gray.200"} width={"50vw"} as="Center" p={"10px"} m='5px'>
+        <CloseIcon onClick={close} display='inline' position={'absolute'} left='95%'p={'2px'}/>
+          <Stack>
+            <Box>
+              <FormLabel display={"inline"}>Aadhar Number</FormLabel>
+              <Input
+                type={"number"}
+                placeholder="Aadhar number"
+                w="20vw"
+                bgColor={"white"}
+              />
+            </Box>
+  
+            <br />
+            <Box>
+              <FormLabel display={"inline"}>Upload image</FormLabel>
+              <Input type={"file"} w="20vw" />
+            </Box>
+          </Stack>
+          <Button colorScheme="facebook" w={"10vw"} type="submit" my={'2px'}>
+            Submit
+          </Button>
+        </FormControl>
+      </>
+    );
+  }
+  
