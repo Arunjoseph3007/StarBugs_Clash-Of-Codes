fdebcb38e277d57986ab1126d424c8e98c587b2a
Anup Joseph <arunjoseph3007@gmail.com>
2023-03-05 02:33:40 +0530

    Create plan api integration

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -2,11 +2,13 @@ import { Input, Box, Button, Center } from "@chakra-ui/react";
 import { useState } from "react";
 import SearchPageGrid from "../components/SearchPageGrid";
 import SearchPageGroups from "../components/SearchPageGroups";
+import Navbar from "@/components/Navbar";
 
-export default function searchPage() {
+export default function SearchPage() {
   const [isLocation, setIsLocation] = useState(true);
   return (
     <Box colorScheme="facebook" p="10px">
+      <Navbar/>
       <Center>
         <Input placeholder="Type here to search" w="40vw" />
         <Button colorScheme={"facebook"} m="10">
